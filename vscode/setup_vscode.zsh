acbc6c68796a3e111d23ce989b48d9ddf106d98d
Ben Patterson <7127765+bmizepatterson@users.noreply.github.com>
2021-02-03 22:13:27 -0600

    Symlink vscode settings

--- vscode/setup_vscode.zsh
+++ vscode/setup_vscode.zsh
@@ -2,4 +2,4 @@
 
 echo "\n<<< Installing Visual Studio Code Extensions >>>\n"
 
-cat vscode_extensions | xargs -n 1 code --install-extension
+cat vscode/vscode_extensions | xargs -n 1 code --install-extension
